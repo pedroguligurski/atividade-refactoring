df115685511e2ee2dba911d15c19c7abea1bea9c
Pedro Henrique Silva Guligurski <142601594+pedroguligurski@users.noreply.github.com>
2024-11-25 18:55:17 -0300

    Update - Rename parameter to aRental - Customer.java

--- arquivos-atividade/Customer.java
+++ arquivos-atividade/Customer.java
@@ -7,41 +7,37 @@ public String statement() {
     while (rentals.hasMoreElements()) {
         Rental each = (Rental) rentals.nextElement();
 
-        // Use o novo método
         double thisAmount = amountFor(each);
-
-        // add frequent renter points
+        
         frequentRenterPoints++;
         if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
             each.getDaysRented() > 1) frequentRenterPoints++;
 
-        // show figures for this rental
         result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
         totalAmount += thisAmount;
     }
 
-    // add footer lines
     result += "Amount owed is " + totalAmount + "\n";
     result += "You earned " + frequentRenterPoints + " frequent renter points";
     return result;
 }
 
-private double amountFor(Rental each) {
+private double amountFor(Rental aRental) {
     double thisAmount = 0;
 
-    switch (each.getMovie().getPriceCode()) {
+    switch (aRental.getMovie().getPriceCode()) {
         case Movie.REGULAR:
             thisAmount += 2;
-            if (each.getDaysRented() > 2)
-                thisAmount += (each.getDaysRented() - 2) * 1.5;
+            if (aRental.getDaysRented() > 2)
+                thisAmount += (aRental.getDaysRented() - 2) * 1.5;
             break;
         case Movie.NEW_RELEASE:
-            thisAmount += each.getDaysRented() * 3;
+            thisAmount += aRental.getDaysRented() * 3;
             break;
         case Movie.CHILDRENS:
             thisAmount += 1.5;
-            if (each.getDaysRented() > 3)
-                thisAmount += (each.getDaysRented() - 3) * 1.5;
+            if (aRental.getDaysRented() > 3)
+                thisAmount += (aRental.getDaysRented() - 3) * 1.5;
             break;
     }
     return thisAmount;
